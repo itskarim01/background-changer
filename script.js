ebe7704470889a49752b0fcfafe231e698471743
itskarim01 <abbashehu775@gmail.com>
2025-04-29 02:03:57 +0100

    Add initial HTML, CSS, and JavaScript files for Background Changer project

---
+++ script.js
@@ -0,0 +1,16 @@
+// ASSIGNMENT 
+
+// Create a website than changes background every 3 seconds 
+let color = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
+
+setInterval(() => {
+    let randomColor = Math.floor(Math.random() * color.length)
+    document.body.style.backgroundColor = color[randomColor]
+}, 2000);
+
+// setTimeout to display hello worid every to see the alert after 3 seconds
+function message() {
+    alert('Hello World');
+}
+
+setTimeout(message, 3000)
